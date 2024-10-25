ab44033fa41190458ccf7322db499cb9b1e05e5c
Gena230295 <gena23solov@gmail.com>
2024-10-25 15:10:57 +0300

    feat: add two tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -285,8 +285,9 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  const newArr = arr.map((a, i) => new Array(i + 1).fill(a)).flat();
+  return newArr;
 }
 
 /**
@@ -338,8 +339,21 @@ function getPositivesCount(arr) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const newArr = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  const finArr = arr.sort((a, b) => newArr.indexOf(a) - newArr.indexOf(b));
+  return finArr;
 }
 
 /**
